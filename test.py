dcf76e3e7503e511b2dd3042754ba95d42dce802
yungchih0209 <chih105015@gmail.com>
2023-08-14 19:24:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,16 +15,14 @@ class TestDataset(Dataset):
     def __init__(self):
         # data loading
         xy = np.loadtxt(TEST_FILE,delimiter=",",dtype=np.float32 ,skiprows=1)
-        self.img = torch.from_numpy(xy[:, 1:].reshape(-1,28,28))
-        self.label = torch.from_numpy(xy[:, 0])
+        self.img = torch.from_numpy(xy.reshape(28,28))
         self.n_samples =xy.shape[0]
         
     def __getitem__(self,index):
-        return self.img[index],self.label[index]
+        return self.img[index]
     
     def __len__(self):
         return self.n_samples
-
 test = TestDataset()
 test_loader = DataLoader(dataset=test ,batch_size = batch_size,shuffle =True,num_workers=0)
 
@@ -32,14 +30,14 @@ model = ConvNeuralNet()
 
 if os.path.exists(MODEL_FILE):
     model =model.load_state_dict(torch.load(MODEL_FILE))
+    model.eval()
 
 
+## 
 with torch.no_grad():
     n_correct =0
     n_samples =0
     for images , labels in test_loader:
-        images = images.unsqueeze(1)
-        labels =labels
         outputs =model(images)
         # max return (value , index)
         _,predicted = torch.max(outputs,1)
