77b5a3b22269ee472c7a186cc7f26873bea7fdad
yungchih0209 <chih105015@gmail.com>
2023-08-26 16:01:54 +0800

    Update CNN

--- Train.py
+++ Train.py
@@ -39,20 +39,22 @@ test_loader =DataLoader(dataset=test_data , batch_size =batch_size,shuffle =Fals
 class ConvNeuralNet(nn.Module):
     def __init__(self,num_classes=10):
         super(ConvNeuralNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 3) 
+        # output_size = (input_size + 2 * padding - kernel_size) / stride + 1
+        self.conv1 = nn.Conv2d(1, 16, 5)  #28-5+1 =24
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(16, 32,3)
-        self.fc1 = nn.Linear(32*5*5, 128)
+        self.conv2 = nn.Conv2d(16,32, 5)
+        self.fc1 = nn.Linear(32*4*4, 128)
         self.fc2 = nn.Linear(128, num_classes)
         
     def forward(self,x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.pool(x)
-        x = self.conv2(x)
-        x = self.relu(x)
-        x = self.pool(x)
+        x = self.conv1(x) # 24
+        x = self.relu(x) 
+        x = self.pool(x) # 12
+        x = self.conv2(x) # 8
+        x = self.relu(x) 
+        x = self.pool(x) # 4
+        # print(x.shape)
         x = x.view(x.size(0),-1) 
         x = self.fc1(x)
         x = self.relu(x)
@@ -63,7 +65,7 @@ model = ConvNeuralNet(num_classes)
 
 def train():
     criterion =nn.CrossEntropyLoss()
-    optimizer =torch.optim.Adam(model.parameters(),lr =0.01)
+    optimizer =torch.optim.Adam(model.parameters(),lr =0.001)
 
     n_total_steps = len(train_loader)
     for epoch in range(num_epochs):
@@ -77,7 +79,7 @@ def train():
             optimizer.step()
             
             if(i+1) % 4000 ==0:
-                print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+                print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.8f}')
     print("Finish Training.")
     torch.save(model.state_dict(),MODEL_FILE)
     
